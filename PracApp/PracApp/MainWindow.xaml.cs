c153031c561d8448e96860453b42fe6ab5fd5598
Toshkent <160218033+AntoshaSenior@users.noreply.github.com>
2025-06-17 12:51:28 +0300

    firstAdd

---
+++ PracApp/PracApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.ComponentModel;
+using System.Windows;
+
+namespace PracApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void RestoreWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Show();
+            WindowState = WindowState.Normal;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            TrayIcon.Dispose();
+            Application.Current.Shutdown();
+        }
+
+
+    }
+}
